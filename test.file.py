db029bfb15fa88d5fe01c98408efa64c702af2ed
Apurwa5 <58842337+Apurwa5@users.noreply.github.com>
2020-06-12 23:10:45 -0700

    Add files via upload

---
+++ test.file.py
@@ -0,0 +1,12 @@
+def test_my_choice():
+    assert isinstance(my_choice('Do you wish to encrypt or decrypt a message?'),str)
+
+
+def test_my_message:
+    assert isinstance(my_message, str) 
+
+
+
+
+
+
